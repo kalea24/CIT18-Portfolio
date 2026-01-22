2bfb63c66d84485853e6a38ffe487d1a1ae30447
cali <cac3683@students.uc-bcf.edu.ph>
2026-01-23 00:58:30 +0800

    added: phptos, linkedin and other portfolio variables

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ const observer = new IntersectionObserver((entries) => {
 document.querySelectorAll('section').forEach(section => {
     section.style.opacity = 0;
     section.style.transform = 'translateY(20px)';
-    section.style.transition = 'opacity 0.6s, transform 0.6s';
+    section.style.transition = 'opacity 1.6s, transform 0.6s';
     observer.observe(section);
 });
 
